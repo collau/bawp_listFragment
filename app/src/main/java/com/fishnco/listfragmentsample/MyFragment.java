a69050299a90fddbd07df949f0f639618dc831d5
junyi <collau@hotmail.com>
2021-06-26 02:14:53 +0800

    1. Intro to List Fragments

---
+++ app/src/main/java/com/fishnco/listfragmentsample/MyFragment.java
@@ -0,0 +1,26 @@
+package com.fishnco.listfragmentsample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by junyi on 26/6/21
+ */
+public class MyFragment extends Fragment {
+    public MyFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.my_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/fishnco/listfragmentsample/data/Course.java
@@ -0,0 +1,36 @@
+package com.fishnco.listfragmentsample.data;
+
+import android.content.Context;
+
+/**
+ * Created by junyi on 26/6/21
+ */
+public class Course {
+    private String courseName;
+    private String courseImage;
+
+    public Course(String courseName, String courseImage) {
+        this.courseName = courseName;
+        this.courseImage = courseImage;
+    }
+
+    public int getImageResourceId(Context context) {
+        return context.getResources().getIdentifier(this.courseName, "drawable", context.getPackageName());
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseImage() {
+        return courseImage;
+    }
+
+    public void setCourseImage(String courseImage) {
+        this.courseImage = courseImage;
+    }
+}
