ee356e5c5c8d4c2fbcce99e65fe10ed98fedcd5f
junyi <collau@hotmail.com>
2021-06-28 17:59:52 +0800

    7. Finalizing Fragment App - Display two panels on Tablet

--- app/src/main/java/com/fishnco/listfragmentsample/MainActivity.java
+++ app/src/main/java/com/fishnco/listfragmentsample/MainActivity.java
@@ -9,13 +9,21 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.fishnco.listfragmentsample.data.Course;
+import com.fishnco.listfragmentsample.util.ScreenUtility;
 
 public class MainActivity extends AppCompatActivity implements CourseListFragment.Callbacks {
+
+    private boolean isTwoPane = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // R.id.detailContainer only exists on large screen
+        if (findViewById(R.id.detailContainer) != null) {
+            isTwoPane = true;
+        }
 
         /*
         // commented after fragment has been inserted into xml
@@ -35,8 +43,19 @@ public class MainActivity extends AppCompatActivity implements CourseListFragmen
     public void onItemSelected(Course course, int position) {
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent(MainActivity.this, CourseDetailActivity.class);
-        intent.putExtra("course_id", position);
-        startActivity(intent);
+        if (isTwoPane) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("course_id", position);
+            FragmentManager fm = getSupportFragmentManager();
+            CourseDetailFragment courseDetailFragment = new CourseDetailFragment();
+            courseDetailFragment.setArguments(bundle);
+
+            fm.beginTransaction().replace(R.id.detailContainer, courseDetailFragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(MainActivity.this, CourseDetailActivity.class);
+            intent.putExtra("course_id", position);
+            startActivity(intent);
+        }
     }
 }
